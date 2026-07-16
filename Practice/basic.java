955ecb4325ea2d5777517b270f13b09d250570a7
Suraj kumar <ksurajkumar336@gmail.com>
2026-07-16 20:29:16 +0530

    comit

---
+++ Practice/basic.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import java.util.*;
+
+/**
+ * basic
+ */
+public class basic {
+  public static void main(String[]args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println("Sum of two number is : " + sum);
+  }
+  
+}
